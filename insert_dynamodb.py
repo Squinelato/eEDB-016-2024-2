a8c50a7637e0bca593e38a594cc4060b056728a7
João Squinelato - Via Consulting <35571896+Squinelato@users.noreply.github.com>
2024-06-28 18:53:51 -0300

    feat: adding batch logic and requirements file

--- insert_dynamodb.py
+++ insert_dynamodb.py
@@ -1,29 +1,44 @@
 from typing import NoReturn
 from decimal import Decimal
+from more_itertools import batched
 
 import argparse
 import boto3
 import json
 
 def insert(table_name: str, file_path: str) -> NoReturn:
+    """
+    Insert a list of JSON items into a DynamoDB table using the batch
+    write operator in chunks of 25 items each.
 
-    print(f"criando cliente DynamoDB")
+    Keyword arguments:
+    table_name -- The name of the DynamoDB table
+    file_path -- the path to the file containing a JSON list of items
+    """
+    print(f"Criating DynamoDB client")
     dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
     table = dynamodb.Table(table_name)
 
-    print(f"abrindo arquivo '{file_path}'")
+    print(f"Opening file '{file_path}'")
     with open(file_path, 'r', encoding='utf-8') as file:
 
         json_list = json.load(file, parse_float=Decimal)
 
-        with table.batch_writer() as batch:
+        batches = batched(json_list, 25)
+        print(f"Splitting items into {len(batches)} batches of 25 items")
 
-            print(f"inserindo itens na tabela '{table_name}': ", end="")
-            for json_item in json_list:
-                print(".", end="")
-                batch.put_item(Item=json_item)
+        for index, batch in enumerate(batches):
 
-        print(f"\n{len(json_list)} itens inseridos com sucesso!")
+            print(f"Inserting {index+1}º batch with {len(batch)} items")
+
+            with table.batch_writer() as batch_writer:
+
+                print(f"Inserting items into '{table_name}' table: ", end="")
+                for item in batch:
+                    print(".", end="")
+                    batch_writer.put_item(Item=item)
+
+        print(f"\nItems inserted successfully!")
 
 if __name__ == "__main__":
 
